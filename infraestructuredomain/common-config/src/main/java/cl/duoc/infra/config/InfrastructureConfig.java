dc5fff0bb448a04bac37e0be533f7dc2092a1aba
Ale-Venegas-L <aleja.venegas@duocuc.cl>
2026-05-08 15:02:17 -0400

    Inicio

---
+++ infraestructuredomain/common-config/src/main/java/cl/duoc/infra/config/InfrastructureConfig.java
@@ -0,0 +1,8 @@
+package cl.duoc.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InfrastructureConfig {
+    // Add shared infrastructure beans or configuration here
+}
